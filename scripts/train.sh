96382a02be8831a65126a4549f9c9176eb227889
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-12-22 21:39:02 +0100

    Create train.sh

---
+++ scripts/train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=baidu-bert
+#SBATCH --time=2-00:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 64GB
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=p.k.hager@uva.nl
+
+source ${HOME}/.bashrc
+mamba activate baidu-bert-model
+
+python main.py
