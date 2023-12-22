89f2b0714e5e7c57ffde58e48319b524f9ac52df
Philipp Hager <philipp.konstantin.hager@gmail.com>
2023-12-22 20:16:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import BertConfig
+from transformers import BertConfig, Trainer, TrainingArguments, EarlyStoppingCallback
 
 import wandb
 from src.const import SPECIAL_TOKENS, SEGMENT_TYPES, MASKING_RATE, MAX_SEQUENCE_LENGTH
@@ -15,8 +15,16 @@ from src.model import MonoBERT
 def main():
     wandb.init(project="baidu-bert-test")
 
-    dataset = BaiduTrainDataset(
+    train_dataset = BaiduTrainDataset(
         Path("data/part-00000.gz"),
+        MAX_SEQUENCE_LENGTH,
+        MASKING_RATE,
+        SPECIAL_TOKENS,
+        SEGMENT_TYPES,
+    )
+
+    eval_dataset = BaiduTrainDataset(
+        Path("data/part-00001.gz"),
         MAX_SEQUENCE_LENGTH,
         MASKING_RATE,
         SPECIAL_TOKENS,
@@ -25,26 +33,32 @@ def main():
 
     config = BertConfig(
         vocab_size=22_000,
-        hidden_size=100,
-        num_hidden_layers=4,
-        num_attention_heads=4,
-        intermediate_size=100,
+        num_hidden_layers=12,
+        num_attention_heads=12,
     )
 
-    data_loader = DataLoader(dataset, batch_size=32)
     model = MonoBERT(config)
     torch.compile(model)
-    optimizer = AdamW(model.parameters(), lr=0.0001)
 
-    for epoch in range(1):
-        for inputs in tqdm(data_loader):
-            loss, query_document_embedding = model(**inputs)
-            loss.backward()
+    args = TrainingArguments(
+        output_dir="output",
+        report_to=["wandb"],
+        run_name="baidu-bert-test",
+        logging_steps=100,
+        evaluation_strategy="steps",
+        eval_steps=1_000,
+        max_steps=1_000_000,
+        seed=0,
+    )
+    trainer = Trainer(
+        model=model,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        args=args,
+        callbacks=[EarlyStoppingCallback(3)],
+    )
 
-            optimizer.step()
-            optimizer.zero_grad()
-
-            wandb.log({"train/loss": loss})
+    trainer.train()
 
 
 if __name__ == "__main__":
