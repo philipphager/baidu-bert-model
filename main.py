5f1295130314f1d0da4d09fa8d041d205f6a987a
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-02 15:19:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import torch
 from pathlib import Path
 
 import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
-from transformers import Trainer, EarlyStoppingCallback, TrainingArguments
+from transformers import Trainer, EarlyStoppingCallback
 
+import wandb
 from src.const import SPECIAL_TOKENS, SEGMENT_TYPES, MASKING_RATE, MAX_SEQUENCE_LENGTH
 from src.data import BaiduTrainDataset
 from src.model import MonoBERT
@@ -13,6 +13,8 @@ from src.model import MonoBERT
 
 @hydra.main(version_base="1.3", config_path="config", config_name="config")
 def main(config: DictConfig):
+    wandb.init(project=config.project)
+
     directory = Path(config.dataset_directory)
     train_files = [f for f in directory.glob("part-*") if f.name != "part-00000.gz"]
     eval_files = [directory / Path("part-00000.gz")]
@@ -33,12 +35,10 @@ def main(config: DictConfig):
         SEGMENT_TYPES,
     )
 
-    TrainingArguments
-
     bert_config = instantiate(config.bert_config)
     training_arguments = instantiate(config.training_arguments)
+
     model = MonoBERT(bert_config)
-    torch.compile(model)
 
     trainer = Trainer(
         model=model,
