46da6b1425c5df259aed1bfdc37a73de73893124
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-04 17:10:34 +0100

    Add pretraining script

---
+++ scripts/pretrain-bert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=pretrain-bert
+#SBATCH --time=4-00:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 32GB
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:4
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=p.k.hager@uva.nl
+
+source ${HOME}/.bashrc
+mamba activate baidu-ultr-features
+
+torchrun --nproc_per_node=4 main.py +training_arguments.run_name=bert-12l-12h-mlm
