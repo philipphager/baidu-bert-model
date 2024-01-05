96e823b2470f0f1b648cb3858ed81e58a508bb4f
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-01-05 16:34:19 +0100

    Create bert-mlm-ctr.sh

---
+++ scripts/bert-mlm-ctr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=mlm-ctr
+#SBATCH --time=2-00:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 32GB
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:4
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=p.k.hager@uva.nl
+
+source ${HOME}/.bashrc
+mamba activate baidu-ultr-features
+
+model="bert-12l-12h-mlm-ctr"
+
+torchrun --nproc_per_node=4 \
+  model=cross-encoder \
+  mask_query=False \
+  mask_doc=True \
+  training_arguments.output_dir="output/${model}" \
+  +training_arguments.run_name="${model}"
